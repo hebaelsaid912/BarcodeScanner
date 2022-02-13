a8ca4378c794b8c5702bec43a0d92bc4c2e13e04
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2022-02-13 16:58:38 +0200

    Formatting

--- app/src/main/java/com/example/android/barcodescanner/data/dao/ItemsDao.kt
+++ app/src/main/java/com/example/android/barcodescanner/data/dao/ItemsDao.kt
@@ -1,12 +1,17 @@
 package com.example.android.barcodescanner.data.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.android.barcodescanner.pojo.Items
 
 @Dao
 interface ItemsDao {
+
     @Query("SELECT * FROM items ORDER By id DESC")
     suspend fun getAllItems():List<Items>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNewItem(item: Items)
 
